3e6ee43270a7db7bfde7c9a5068eddfe243afa3a
willhughes11 <willhughes2110@gmail.com>
2020-09-07 17:37:44 -0400

    master

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import os
 SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
+#DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = 'postgres://pimpmasterguapo@localhost:5432/castingapp'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
