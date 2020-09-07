a349010bed3f77b85299e3b5fb03325b617922d6
willhughes11 <willhughes2110@gmail.com>
2020-09-07 18:32:20 -0400

    master

--- config/config.py
+++ config/config.py
@@ -5,6 +5,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 #DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'postgres://pimpmasterguapo@localhost:5432/castingapp'
+SQLALCHEMY_DATABASE_URI = 'postgres://mackifhwaktlxc:a1b30a371d83adcc4df298f71b7041a709a7fc4c0dcf2e97c0c45f00153e099f@ec2-23-20-168-40.compute-1.amazonaws.com:5432/de40etook3v8b'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 DEBUG_TB_INTERCEPT_REDIRECTS = False
